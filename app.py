cc05865be48736b27435ac920f4c8c87a2f32e63
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2013-06-07 10:24:46 +0000

    More fleshing out of templates

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.debug = True
 
-
-
 @app.route('/')
 def home():
-    return render_template('home.html')
+  return render_template('home.html')
+
+@app.route('/cv/')
+def cv():
+  return render_template('cv.html')
+
+@app.route('/contact/')
+def contact():
+  return render_template('contact.html')
+
+@app.route('/projects/')
+def projects():
+  return render_template('projects.html')
+
+@app.route('/projects/parxiv/')
+def parxiv():
+  
+  return render_template('projects/parxiv.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0')
