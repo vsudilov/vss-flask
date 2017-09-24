7073decab5b42e6f254bf47ab647bc8ea5f1b264
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2017-09-23 23:09:17 -0400

    deploy: fix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-SYNC="css fonts images index.html js"
+set -xe
+SYNC="css fonts images"
 for i in ${SYNC}; do
-  aws s3 sync ${i} s3://www.vsudilovsky.com/
+  aws s3 sync ${i} s3://www.vsudilovsky.com/${i}
 done
+
+aws s3 cp index.html s3://www.vsudilovsky.com/index.html
